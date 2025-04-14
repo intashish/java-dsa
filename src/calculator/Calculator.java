7c313b1b721db02083c62a12a7c0bd03c73f6bf3
ashish <ashishsingh.int@gmail.com>
2025-04-14 21:55:07 +0530

    reverting last commit

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -2,11 +2,12 @@ package calculator;
 
 public class Calculator {
 
+
 	void sub(int a, int b) {
 		System.out.println(a-b);
 	}
 	
-	void add(int c, int d) {
-		System.out.println(c+d);
+	void add(int a, int b) {
+		System.out.println(a+b);
 	}
 }
